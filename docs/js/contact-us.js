7eb58acea9102f7d3a606222e71e45da0f8dd14d
Ahmed Bayoumy <bayoumy.squash@gmail.com>
2018-05-29 13:18:53 -0400

    removing the docs folder after realzing it's not neccassary

--- docs/js/contact-us.js
+++
@@ -1,5 +0,0 @@
-document.onload(function(){
-    var iframe = document.getElementById('google-form1');
-    var content = iframe.contentDocument || iframe.contentWindow.document;
-    content.body.style.backgroundColor = "black";
-})
